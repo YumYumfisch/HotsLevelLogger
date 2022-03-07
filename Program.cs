36ff4e2d073400678ff1993f2bea466e71d1cf37
hc <hc@YUMYUMFISCH>
2022-03-07 16:36:30 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hots_Level_Logger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

53a969e0f83f1dc6dbca8a92a8b03f0ba1490199
YumYumFisch <YumYumFisch#7352>
2022-03-12 12:00:33 +0100

    Accounting for shifting of digits by analyzing larger area and adding extra padding

--- ImageManipulation.cs
+++ ImageManipulation.cs
@@ -12,41 +12,47 @@ namespace Hots_Level_Logger
         /// <summary>
         /// Sets every pixel to black if it has a valid color.
         /// Otherwise it will be set to white.
+        /// Adds additional padding between digits.
         /// </summary>
-        /// <param name="bitmap">Image to be manipulated.</param>
+        /// <param name="source">Image to be manipulated.</param>
         /// <returns>Binarized (Black and White) bitmap with numbers in black on a white background.</returns>
-        public static Bitmap ImageCleanup(Bitmap bitmap)
+        public static Bitmap ImageCleanup(Bitmap source)
         {
-            for (int x = 0; x < bitmap.Width; x++)
+            Bitmap output = new Bitmap(source.Width + 8, source.Height);
+
+            for (int x = 0; x < output.Width; x++)
             {
-                for (int y = 0; y < bitmap.Height; y++)
+                for (int y = 0; y < output.Height; y++)
                 {
-                    // Remove Border (2 lines each at the top and bottom and 3 rows each at the left and right)
-                    if (y <= 2 || y >= bitmap.Height - 2 || x <= 3 || x >= bitmap.Width - 3)
+                    // Remove border (2 lines each at the top and bottom and 3 rows each at the left and right)
+                    if (y < 2 || y >= output.Height - 2 || x < 3 || x >= output.Width - 3)
                     {
-                        bitmap.SetPixel(x, y, Color.White);
+                        output.SetPixel(x, y, Color.White);
                         continue;
                     }
 
-                    // Remove padding between digits
-                    if (x > 10 && (x - 10) % 9 < 3)
+                    // Add extra padding between digits
+                    if ((x - 3) % 11 >= 9)
                     {
-                        bitmap.SetPixel(x, y, Color.White);
+                        output.SetPixel(x, y, Color.White);
                         continue;
                     }
 
+                    // Add digits
+                    int digitIndex = (x - 3) / 11; // Ignoring border padding and accounting for digit width (7p plus one pixel of extra space each for error corection and one for padding on each side)
+                    int pixelShift = digitIndex * 2 + 1; // Accounting for extra padding between digits
                     // Remove invalid pixels in the area of the digit
-                    if (ValidatePixelColor(bitmap.GetPixel(x, y)))
+                    if (ValidatePixelColor(source.GetPixel(x - pixelShift, y)))
                     {
-                        bitmap.SetPixel(x, y, Color.Black);
+                        output.SetPixel(x, y, Color.Black);
                     }
                     else
                     {
-                        bitmap.SetPixel(x, y, Color.White);
+                        output.SetPixel(x, y, Color.White);
                     }
                 }
             }
-            return bitmap;
+            return output;
         }
 
         /// <summary>
